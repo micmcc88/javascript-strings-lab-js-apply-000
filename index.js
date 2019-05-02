e5fdb8334ddf8cc06ce587052f63ec3b49f54ddd
Michael McCabe <micmcc2001@hotmail.com>
2019-05-02 17:06:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var space0 = "Let"
+
+var apos = "''"
+
+var space1 = $apos + "s talk about space."
+
+var space = $space0 + space1
 
 var topic = "space";
 
